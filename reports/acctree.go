8495695e02db4276458fd822b52bd29fa49834bd
prataprc <prataprc@gmail.com>
2017-05-13 13:08:36 +0530

    account tree.

--- reports/acctree.go
+++ reports/acctree.go
@@ -1,31 +1,33 @@
 package reports
 
-//import "github.com/tn47/goledger/dblentry"
-//
-//type accnode struct {
-//	name     string
-//	children []*accnode
-//}
-//
-//func newaccnode(name string) *accnode {
-//	return &accnode{name: name, children: make([]*accnode, 0)}
-//}
-//
-//func (an *accnode) adddescendants(names []string) {
-//	if len(names) == 0 {
-//		return
-//	}
-//	child, ok := an.children[names[0]]
-//	if ok == false {
-//		child = newaccnode(names[0])
-//	}
-//	return child.adddescendants(names[1:])
-//}
-//
-//func accpath2tree(accnames []string) *accnode {
-//	root := newaccnode("__root__")
-//	for _, accname := range accnames {
-//		root.adddescendants(dblentry.SplitAccount(accname))
-//	}
-//	return root
-//}
+import "github.com/tn47/goledger/dblentry"
+
+type accnode struct {
+	name     string
+	children map[string]*accnode
+}
+
+func newaccnode(name string) *accnode {
+	return &accnode{name: name, children: make(map[string]*accnode)}
+}
+
+func (an *accnode) adddescendants(names []string) {
+	if len(names) == 0 {
+		return
+	}
+	child, ok := an.children[names[0]]
+	if ok == false {
+		child = newaccnode(names[0])
+	}
+	an.children[names[0]] = child
+	child.adddescendants(names[1:])
+	return
+}
+
+func accpath2tree(accnames []string) *accnode {
+	root := newaccnode("__root__")
+	for _, accname := range accnames {
+		root.adddescendants(dblentry.SplitAccount(accname))
+	}
+	return root
+}
